6ae45d69300f18a04ddbc1631e67088842a5c99a
Thales Jose <thalesjoseaguiar@gmail.com>
2023-09-26 12:42:31 -0300

    criacao da exception EnderecoException

---
+++ src/main/java/br/com/attornatusdesafio/services/exceptions/EnderecoException.java
@@ -0,0 +1,12 @@
+package br.com.attornatusdesafio.services.exceptions;
+
+public class EnderecoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EnderecoException(String message) {
+		super(message);
+		
+	}
+
+}
