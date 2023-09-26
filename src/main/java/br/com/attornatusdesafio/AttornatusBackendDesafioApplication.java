e1c5e0b0e6a9a8288c4dc92776b7c73c09f6d74b
Thales Jose <thalesjoseaguiar@gmail.com>
2023-09-26 11:24:20 -0300

    first commit

---
+++ src/main/java/br/com/attornatusdesafio/AttornatusBackendDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatusdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusBackendDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusBackendDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatusdesafio/AttornatusBackendDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatusdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusBackendDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
