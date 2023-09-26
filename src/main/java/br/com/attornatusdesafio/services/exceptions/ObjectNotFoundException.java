27c95c3670eabe9480f7d4fea238113090f6510d
Thales Jose <thalesjoseaguiar@gmail.com>
2023-09-26 12:40:12 -0300

    criacao da exception ObjectNotFoundException

---
+++ src/main/java/br/com/attornatusdesafio/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.attornatusdesafio.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String message) {
+		super(message);
+		
+	}
+}
