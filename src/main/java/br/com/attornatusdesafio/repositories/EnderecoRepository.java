44abe2d4f408fe93b9bde147713181d3e5bb14e2
Thales Jose <thalesjoseaguiar@gmail.com>
2023-09-26 14:09:43 -0300

    criacao de query para procurar enderecos especifico de pessoa

--- src/main/java/br/com/attornatusdesafio/repositories/EnderecoRepository.java
+++ src/main/java/br/com/attornatusdesafio/repositories/EnderecoRepository.java
@@ -1,5 +1,17 @@
 package br.com.attornatusdesafio.repositories;
 
-public interface EnderecoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.attornatusdesafio.entities.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
+
+	
+	@Query("SELECT e FROM Endereco e WHERE e.id IN (SELECT endereco.id FROM Pessoa p JOIN p.enderecos endereco WHERE p.id = :idpessoa)")
+	List<Endereco> findAllByPessoa(@Param(value = "idpessoa") Long idPessoa);
 
 }
